ee321c483891d13c06a21d9693fc716639165d72
himanshu2312 <himanshu2312gupta@gmail.com>
2023-07-17 16:15:32 +0530

    Page load, Next or previous button feature added

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -2,40 +2,80 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-
   constructor() {
     super();
-    console.log("I am a constructor of news component")
+    // console.log("I am a constructor of news component")
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
+    console.log(this.state.page);
   }
 
-  async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=53fcb599db2d40d4b6517951bd93de65";
-    let data = await fetch(url);
-    let parseData= await data.json();
-    console.log(parseData)
-    this.setState({articles: parseData.articles})
+  componentDidMount() {
+    this.loadPage(0);
   }
+
+  loadPage = async (n) => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=53fcb599db2d40d4b6517951bd93de65&page=${
+      this.state.page + n
+    }`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    // console.log(parseData);
+    this.setState({ articles: parseData.articles, page: this.state.page + n , totalResults: parseData.totalResults });
+  };
+
+  handleNext = () => {
+    this.loadPage(1);
+  };
+
+  handlePre = () => {
+    this.loadPage(-1);
+  };
+
   render() {
     return (
-      <div className="container" >
-        <h1><center>NewsTechs - Top headlines</center></h1>
+      <div className="container">
+        <h1>
+          <center>NewsTechs - Top headlines</center>
+        </h1>
         <div className="row">
           {this.state.articles.map((ele) => {
             return (
               <div className="col-md-4 my-3" key={ele.url}>
                 <NewsItem
-                  title={ele.title?ele.title:""}
-                  description={ele.description?ele.description:""}
-                  img={ele.urlToImage?ele.urlToImage:"https://cdn0.iconfinder.com/data/icons/ui-3-1/512/news-128.png"}
+                  title={ele.title ? ele.title : ""}
+                  description={ele.description ? ele.description : ""}
+                  img={
+                    ele.urlToImage
+                      ? ele.urlToImage
+                      : "https://cdn0.iconfinder.com/data/icons/ui-3-1/512/news-128.png"
+                  }
                   newsurl={ele.url}
                 />
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            id="btnPre"
+            className="btn btn-secondary "
+            disabled={this.state.page <= 1}
+            onClick={this.handlePre}
+          >
+            &larr; Previous
+          </button>
+          <button
+            id="btnNext"
+            className="btn btn-primary "
+            onClick={this.handleNext}
+            disabled={Math.ceil(this.state.totalResults/20)===this.state.page}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-  constructor(){
-    super();
-    console.log("I am a constructor of a newsitem")
-  }
+  
   render() {
     let { title, description , img, newsurl } = this.props;
     return (
@@ -14,7 +11,7 @@ export default class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsurl} target="_blank" className="btn btn-primary">
+            <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-primary">
               Read More
             </a>
           </div>
