cf217786a82413884b1a1faa442e3ad9fa66b7b6
himanshu2312 <himanshu2312gupta@gmail.com>
2023-09-27 15:48:46 +0530

    api update

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API=axios.create({ baseURL: "http://localhost:4000"});
+const API=axios.create({ baseURL: "https://news-app-2312.onrender.com"});
 
 export const fetchNews=(newsData)=>API.post("/news",newsData);
