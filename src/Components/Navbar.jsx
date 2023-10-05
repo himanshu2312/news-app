e3ca43ffa27df32183bc97f58a0bc9305669940b
himanshu2312 <himanshu2312gupta@gmail.com>
2023-10-05 16:22:03 +0530

    News converted to fuction component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = (props) => {
   let { NavbarTitle } = props;
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             <b>{NavbarTitle}</b>
--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,93 +1,95 @@
-import React, { Component } from "react";
+import React, { useEffect,useState } from "react";
 import NewsItem from "./NewsItem";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Spinner from "./Spinner";
 import moment from "moment";
-import {fetchNews} from "../api/index.js";
+import { fetchNews } from "../api/index.js";
 
-export default class News extends Component {
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      page: 0,
-      totalResults: 0,
-      loading: true
-    };
-  }
+const News = (props) => {
+  const [articles, setArticles] = useState([])
+  const [page, setpage] = useState(0);
+  const [totalResults, setTotalResults] = useState(0)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    loadPage();
+    // eslint-disable-next-line
+  }, [])
   
-  componentDidMount() {
-    this.loadPage();
-  }
-  
-  loadPage = async () => {
-    let {pageSize,category, country, setProgress}=this.props
+
+  const loadPage = async () => {
+
+    let { pageSize, category, country, setProgress } = props
     setProgress(10);
-    if(category!=="general"){
-      document.title=`${this.capitalize(category)} | NewsTechs`
+
+    if (category !== "general") {
+      document.title = `${capitalize(category)} | NewsTechs`
     }
-    
     setProgress(20)
-    let result = await fetchNews({category,country,pageSize,page:this.state.page+1})
+
+    let result = await fetchNews({ category, country, pageSize, page: page + 1 })
     setProgress(60);
 
+    setArticles(articles.concat(result.data.articles));
     setProgress(70);
-    this.setState({
-      articles: this.state.articles.concat(result.data.articles),
-      page: this.state.page + 1,
-      totalResults: result.data.totalResults,
-      loading: false
-    });
+    
+    setpage(page+1);
+    setProgress(80);
+    
+    setTotalResults(result.data.totalResults);
+    setProgress(90);
+    
+    setLoading(false);
     setProgress(100);
   };
-  
-  capitalize=(str)=>{
+
+  const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
-  
-  fetchData = () => {
+
+  const fetchData = () => {
     setTimeout(() => {
-      this.loadPage()
+      loadPage()
     }, 1500);
   };
 
-  render() {
 
-    return (
-      <>
-        <h1 style={{margin: '20px'}}>
-          <center>{`NewsTechs - Top headlines ${this.props.category==='general'?"":`on ${this.capitalize(this.props.category)}`}`}</center>
-        </h1>
-        {this.state.loading&&<Spinner/>}
-        <InfiniteScroll 
-          dataLength={this.state.articles.length} //This is important field to render the next data
-          next={()=>{this.fetchData()}}
-          hasMore={this.state.articles.length!==this.state.totalResults}
-          loader={<Spinner/>}
-          >
-          <div className="container">
-            <div className="row" >
-              {this.state.articles?.map((ele,index) => {
-                return (
-                  <div className="col-md-4 my-3" key={index}>
-                    <NewsItem
-                      title={ele.title ? ele.title : ""}
-                      description={ele.description ? ele.description : ""}
-                      img={
-                        ele.urlToImage
-                      }
-                      newsurl={ele.url}
-                      author={ele.author}
-                      date={moment(ele.publishedAt).fromNow()}
-                      source={ele.source.name}
-                      />
-                  </div>
-                );
-              })}
-            </div>
+  return (
+    <>
+      <h1 style={{ margin: '20px', marginTop: "80px" }}>
+        <center>{`NewsTechs - Top headlines ${props.category === 'general' ? "" : `on ${capitalize(props.category)}`}`}</center>
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length} //This is important field to render the next data
+        next={() => { fetchData() }}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row" >
+            {articles?.map((ele, index) => {
+              return (
+                <div className="col-md-4 my-3" key={index}>
+                  <NewsItem
+                    title={ele.title ? ele.title : ""}
+                    description={ele.description ? ele.description : ""}
+                    img={
+                      ele.urlToImage
+                    }
+                    newsurl={ele.url}
+                    author={ele.author}
+                    date={moment(ele.publishedAt).fromNow()}
+                    source={ele.source.name}
+                  />
+                </div>
+              );
+            })}
           </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
+        </div>
+      </InfiniteScroll>
+    </>
+  );
 }
+
+export default News;
