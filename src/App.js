a30b71577e38e04524c1fabe141e99b1a723ad97
himanshu2312 <himanshu2312gupta@gmail.com>
2023-10-05 16:31:02 +0530

    App converted to fuction component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import React, { Component } from "react";
+import {React, useState} from "react";
 import Navbar from "./Components/Navbar";
 import "./App.css";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import AllRoutes from "./AllRoutes";
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-    constructor(){
-      super();
-      this.state = {
-        progress:0
-      }
-    }
-    setProgress=(p)=>{
-      this.setState({
-        progress:p
-      })
-    }
-  render() {
-    return (
-      <div>
-        <Router>
-          <LoadingBar
-            color='#f11946'
-            progress={this.state.progress}
-            onLoaderFinished={() => this.setProgress(0)}
-            />
-          <Navbar NavbarTitle="NewsTechs" />
-          <AllRoutes setProgress={this.setProgress}/>
-        </Router>
-      </div>
-    );
-  }
+export default function App(){
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <Router>
+        <LoadingBar
+          color='#f11946'
+          progress={progress}
+          onLoaderFinished={()=>setProgress(0)}
+        />
+        <Navbar NavbarTitle="NewsTechs" />
+        <AllRoutes setProgress={setProgress} />
+      </Router>
+    </div>
+  );
 }
